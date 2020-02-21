eabd06675adc679dfee2857967df968fce14702b
vla5924 <vla2000@inbox.ru>
2020-02-21 11:53:56 +0300

    Lib

---
+++ src/04_Graph/04_Graph/Lib/TestClass.cpp
@@ -0,0 +1,11 @@
+#include "TestClass.h"
+
+int TestClass::echo(int number)
+{
+    return number;
+}
+
+void TestClass::helloWorld()
+{
+    std::cout << "Hello, World!";
+}
---
+++ src/04_Graph/04_Graph/Lib/TestClass.h
@@ -0,0 +1,12 @@
+#ifndef _TESTCLASS_H_
+#define _TESTCLASS_H_
+#include <iostream>
+
+class TestClass
+{
+public:
+    int echo(int);
+    void helloWorld();
+};
+
+#endif // !_TESTCLASS_H_
