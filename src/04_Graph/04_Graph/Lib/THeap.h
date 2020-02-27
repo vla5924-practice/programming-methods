067935f4b0a2bcf97960656c2493d8cf83b66593
vla5924 <vla2000@inbox.ru>
2020-02-27 16:59:27 +0300

    THeap (заготовка)

--- src/04_Graph/04_Graph/Lib/THeap.h
+++ src/04_Graph/04_Graph/Lib/THeap.h
@@ -1,33 +1,34 @@
 #ifndef _THEAP_H_
 #define _THEAP_H_
 #include <ciso646>
+#include <functional>
 
 template<typename TData>
 class THeap
 {
-    int form;
+public:
+    typedef std::function<int(const TData&)> TWeightFunc;
+    int base;
     int capacity;
     int size;
     int* keys;
     TData* elements;
+    //int(*weight)(const TData&);
+    TWeightFunc weight;
 
     void transpose(int i, int j);
+    int min(int first, int second) const;
+    int getWeight(int i) const;
     int findMinChild(int i) const;
-    void popUp(int i);
+    //void popUp(int i);
     void dipDown(int i);
-    void heaping();
+    void heapify();
 public:
     THeap(const THeap& other);
-    THeap(const TData* const elements_, int size, int form_ = 2);
+    THeap(TData* const elements_, int size_, TWeightFunc weight_, int base_ = 2);
     ~THeap();
-
-    TData* popTop();
+    TData* pullTop();
 };
-
-inline int findMin(int first, int second)
-{
-    return first < second ? first : second;
-}
 
 template<typename TData>
 void THeap<TData>::transpose(int i, int j)
@@ -38,58 +39,57 @@ void THeap<TData>::transpose(int i, int j)
 }
 
 template<typename TData>
-void THeap<TData>::popUp(int i)
+inline int THeap<TData>::min(int first, int second) const
 {
-    int parent = (i - 1) / form;
-    while (i > 0)
-    {
-        if (keys[parent] > keys[i])
-        {
-            transpose(parent, i);
-            i = parent;
-        }
-        else
-            break;
-    }
+    return first < second ? first : second;
 }
 
 template<typename TData>
-void THeap<TData>::dipDown(int i)
+inline int THeap<TData>::getWeight(int i) const
 {
-    int c = findMinChild(i);
-    while ((c != -1) and (keys[c] < keys[i]))
-    {
-        transpose(c, i);
-        i = c;
-        c = findMinChild(i);
-    }
-}
-
-template<typename TData>
-void THeap<TData>::heaping()
-{
-    for (int i = size - 1; i >= 0; i--)
-        dipDown(i);
+    return weight(elements[keys[i]]);
 }
 
 template<typename TData>
 int THeap<TData>::findMinChild(int i) const
 {
-    if (i * form + 1 >= size)
+    if (i * base + 1 >= size)
         return -1;
-    int c1 = i * form + 1;
-    int c2 = findMin(size - 1, i * form + form);
-    int c = c1;
-    for (int i = c1 + 1; i <= c2; i++)
-        if (keys[i] < keys[c])
-            c = i;
-    return c;
+    int firstChild, lastChild, minChild;
+    firstChild = i * base + 1;
+    lastChild = min(size - 1, i * base + base);
+    minChild = firstChild;
+    for (int i = firstChild + 1; i <= lastChild; i++)
+        if (getWeight(i) < getWeight(minChild))
+            minChild = i;
+    return minChild;
+
+}
+
+template<typename TData>
+void THeap<TData>::dipDown(int i)
+{
+    int minChild = findMinChild(i);
+    while (minChild != i)
+    {
+        transpose(i, minChild);
+        i = findMinChild(i);
+    }
+}
+
+template<typename TData>
+void THeap<TData>::heapify()
+{
+    for (int i = size - 1; i >= 0; i--)
+    {
+        dipDown(i);
+    }
 }
 
 template<typename TData>
 THeap<TData>::THeap(const THeap<TData>& other)
 {
-    form = other.form;
+    base = other.base;
     capacity = other.capacity;
     size = other.size;
     keys = new int[capacity];
@@ -99,14 +99,17 @@ THeap<TData>::THeap(const THeap<TData>& other)
 }
 
 template<typename TData>
-THeap<TData>::THeap(const TData* const elements_, int size, int form_)
+THeap<TData>::THeap(TData* const elements_, int size_, TWeightFunc weight_, int base_)
 {
     elements = elements_;
     capacity = size_;
     size = size_;
     keys = new int[capacity];
-    form = form_;
-    // окучивание();
+    for (int i = 0; i < size; i++)
+        keys[i] = i;
+    base = base_;
+    weight = weight_;
+    heapify();
 }
 
 template<typename TData>
@@ -117,13 +120,13 @@ THeap<TData>::~THeap()
 }
 
 template<typename TData>
-TData* THeap<TData>::popTop()
+TData* THeap<TData>::pullTop()
 {
-    TData* element = elements[0];
+    int index = keys[0];
     transpose(0, size - 1);
-    size -= 1;
     dipDown(0);
-    return element;
+    size--;
+    return elements + index;
 }
 
 #endif
