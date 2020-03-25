4b5bf0f40359c300456409c01b77168bee1b8ca1
vla5924 <vla2000@inbox.ru>
2020-03-25 17:08:26 +0300

    mt19937 fix, operators in cpp, C++17

--- src/04_Graph/include/TEdge.h
+++ src/04_Graph/include/TEdge.h
@@ -8,15 +8,18 @@ using TVertexId = int;
 struct TEdge
 {
     TVertexId x, y;
-    int weight = 0;
+    float weight = 0.0f;
+
     TEdge() = default;
-    TEdge(TVertexId x_, TVertexId y_, int weight_);
-    //bool incidental(const TEdge& other) const;
+    TEdge(TVertexId x_, TVertexId y_, float weight_);
+
+    bool incidental(const TEdge& other) const;
     bool incidental(TVertexId vertex) const;
-    bool operator> (const TEdge& edge) const { return weight > edge.weight; }
-    bool operator< (const TEdge& edge) const { return weight < edge.weight; }
-    bool operator>=(const TEdge& edge) const { return weight >= edge.weight; }
-    bool operator<=(const TEdge& edge) const { return weight <= edge.weight; }
+
+    bool operator> (const TEdge& edge) const;
+    bool operator< (const TEdge& edge) const;
+    bool operator>=(const TEdge& edge) const;
+    bool operator<=(const TEdge& edge) const;
 };
 
 #endif
--- src/04_Graph/sample/sort.cpp
+++ src/04_Graph/sample/sort.cpp
@@ -7,7 +7,7 @@ constexpr int size = 10;
 int main()
 {
     std::random_device rd;
-    std::mt19937_64 generator(rd()); // Mersenne
+    std::mt19937 generator(rd()); // Mersenne
 
     int arr[size] = { 0 };
     for (int i = 0; i < size; i++)
