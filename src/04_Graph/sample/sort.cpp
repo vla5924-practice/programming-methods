6f93a4ac0d825ea54d4bb6cba1faebf6dec955d4
vla5924 <vla2000@inbox.ru>
2020-03-25 16:47:16 +0300

    хайповый чуть ли не с++20 рандомайзер

--- src/04_Graph/sample/sort.cpp
+++ src/04_Graph/sample/sort.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <random>
 #include "THeap.h"
+
+constexpr int size = 10;
 
 int main()
 {
-    int arr[] = { 5, 3, 4, -1, 0, 10, 3, 0, 7, 6 };
-    THeap<int> heap(arr, 10);
+    std::random_device rd;
+    std::mt19937_64 generator(rd()); // Mersenne
+
+    int arr[size] = { 0 };
+    for (int i = 0; i < size; i++)
+        arr[i] = static_cast<int>(generator() % 1000U);
+
+    THeap<int> heap(arr, size);
     heap.sort();
+    for (int i = 0; i < size; i++)
+        std::cout << arr[i] << ' ';
 }
